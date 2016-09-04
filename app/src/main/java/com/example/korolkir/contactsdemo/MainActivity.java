765fbc2972933ddfc6d5a8689a6480884503b258
korolkir <korolkir@gmail.com>
2016-09-04 20:00:21 +0300

    add image view with picture

--- app/src/main/java/com/example/korolkir/contactsdemo/MainActivity.java
+++ app/src/main/java/com/example/korolkir/contactsdemo/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.korolkir.contactsdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
